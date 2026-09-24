512b0679d702c755fa86a9e125120e713cbd61f0
Fadhel-docom <kfadhel13@gmail.com>
2026-09-24 16:40:09 +0100

    restore: preserve existing Android project files

---
+++ app/src/main/java/com/homeprojectwise/app/MainActivity.java
@@ -0,0 +1,4 @@
+package com.homeprojectwise.app;
+import android.app.Activity;
+import android.os.Bundle;
+public class MainActivity extends Activity { @Override protected void onCreate(Bundle savedInstanceState){ super.onCreate(savedInstanceState); } }
